8b74f8c1411751af0686e7983d048825c74fe797
Disequilibrium <56952301+Disequilibrium@users.noreply.github.com>
2021-10-27 02:12:02 -0400

    Minimized gitignore, added initial py files

---
+++ src/app/gemini.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Disequilibrium
+# Description: This file serves as the program initializer.
+#              It may be used to set up the working environment as well.
+
+def run():
+    return 0
+
+if __name__ == "__main__":
+    run()
---
+++ src/app/gui.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# Author: Disequilibrium
+# Description: GUI classes and/or instances are defined within this file.
