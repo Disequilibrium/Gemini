8d76c529b6d7888b3cd6425af6b9a27ec3764c3c
Disequilibrium <56952301+Disequilibrium@users.noreply.github.com>
2021-10-31 20:41:32 -0400

    AboutDialog UI implemented

--- src/app/gui.py
+++ src/app/gui.py
@@ -6,13 +6,13 @@ import backend
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
+## MAIN WINDOW CLASS ##
 class Ui_mainWindow(object):
     def setupUi(self, mainWindow):
         mainWindow.setObjectName("mainWindow")
         mainWindow.setWindowModality(QtCore.Qt.NonModal)
         mainWindow.setEnabled(True)
-        mainWindow.resize(370, 316)
+        mainWindow.setFixedSize(370, 330)
         mainWindow.setAnimated(True)
         self.centralwidget = QtWidgets.QWidget(mainWindow)
         self.centralwidget.setAutoFillBackground(False)
@@ -76,6 +76,9 @@ class Ui_mainWindow(object):
         self.retranslateUi(mainWindow)
         QtCore.QMetaObject.connectSlotsByName(mainWindow)
 
+        ## MainWindow Functionalities ##
+        self.actionAbout.triggered.connect(self.openAbout)
+
     def retranslateUi(self, mainWindow):
         _translate = QtCore.QCoreApplication.translate
         mainWindow.setWindowTitle(_translate("mainWindow", "Gemini"))
@@ -90,10 +93,51 @@ class Ui_mainWindow(object):
         self.actionEstablish_LiveSync.setText(_translate("mainWindow", "Establish LiveSync..."))
         self.actionAbout.setText(_translate("mainWindow", "About..."))
 
+    def openAbout(self):
+        AboutDialog = QtWidgets.QDialog()
+        AboutDialog.ui = Ui_AboutDialog()
+        AboutDialog.ui.setupUi(AboutDialog)
+        AboutDialog.exec_()
+        AboutDialog.show()
+
+
+## ABOUT DIALOG CLASS ##
+class Ui_AboutDialog(object):
+    def setupUi(self, AboutDialog):
+        AboutDialog.setObjectName("AboutDialog")
+        AboutDialog.setFixedSize(221, 120)
+        self.label = QtWidgets.QLabel(AboutDialog)
+        self.label.setGeometry(QtCore.QRect(30, 20, 171, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(AboutDialog)
+        self.label_2.setGeometry(QtCore.QRect(30, 40, 171, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(AboutDialog)
+        self.label_3.setGeometry(QtCore.QRect(30, 60, 171, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(AboutDialog)
+        self.label_4.setGeometry(QtCore.QRect(30, 80, 171, 16))
+        self.label_4.setObjectName("label_4")
+
+        self.retranslateUi(AboutDialog)
+        QtCore.QMetaObject.connectSlotsByName(AboutDialog)
+
+    def retranslateUi(self, AboutDialog):
+        _translate = QtCore.QCoreApplication.translate
+        AboutDialog.setWindowTitle(_translate("AboutDialog", "About"))
+        self.label.setText(_translate("AboutDialog", "Version: 1.0"))
+        self.label_2.setText(_translate("AboutDialog", "Created by: Disequilibrium"))
+        self.label_3.setText(_translate("AboutDialog", "Powered by: Python 3"))
+        self.label_4.setText(_translate("AboutDialog", "GUI: PyQt5"))
+
 
 def start():
+
     import sys
+    import qdarkstyle
+
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
     mainWindow = QtWidgets.QMainWindow()
     ui = Ui_mainWindow()
     ui.setupUi(mainWindow)
